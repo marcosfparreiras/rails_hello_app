92bec824edbbbd091c7ea6c8eb4467bb429687e1
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-01-14 16:45:11 -0200

    Create simple Hello World application; Set everything to deploy on Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloApp::Application.config.secret_key_base = '7e29a2e77e7708a52bd73df9d8b1a2059cefedec3723943b1de2af2b0a72fc4bd35ae48bd3ba84de9e6f663d51d89f4036205f44ccd876fc3694f9853fdda570'
